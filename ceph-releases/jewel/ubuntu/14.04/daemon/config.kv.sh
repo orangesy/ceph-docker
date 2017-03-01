9516b6835da3cf877ba0777eba1cc34e23d8ac84
Cooper Su <su.jming@gmail.com>
2017-03-01 14:12:09 +0800

    Hide logs when ETCD loading.

--- ceph-releases/jewel/ubuntu/14.04/daemon/config.kv.sh
+++ ceph-releases/jewel/ubuntu/14.04/daemon/config.kv.sh
@@ -5,7 +5,7 @@ function check_KV_IP {
   : ${K8S_NETWORK:=${CEPH_CLUSTER_NETWORK}}
 
   # search K8S_NETWORK first
-  if K8S_IP=$(ip -4 -o a | awk '{ sub ("/..", "", $4); print $4 }' | grepcidr "$K8S_NETWORK") && \
+  if K8S_IP=$(ip -4 -o a | awk '{ sub ("/..", "", $4); print $4 }' | grepcidr "$K8S_NETWORK" 2>/dev/null) && \
     curl http://"${K8S_IP}":"${KV_PORT}" &>/dev/null; then
     KV_IP=${K8S_IP}
     return 0
@@ -29,6 +29,11 @@ function check_KV_IP {
     log_err "\$KV_IP=${KV_IP}"
     exit 1
   fi
+}
+
+# hide stdout of confd
+function confd {
+  command confd $@ &>/dev/null
 }
 
 sed -r "s/@CLUSTER@/${CLUSTER:-ceph}/g" \
